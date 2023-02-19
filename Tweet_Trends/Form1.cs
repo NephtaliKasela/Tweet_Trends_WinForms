ba3298a774a4366804f861e2fa639331b938890a
Nephtali Kasela <nephtalikasela04@gmail.com>
2023-02-20 00:41:32 +0300

    Make a small change.

--- Tweet_Trends/Form1.cs
+++ Tweet_Trends/Form1.cs
@@ -70,37 +70,8 @@ namespace Tweet_Trends
 
             // Print location of tweets
             listBox1.Items.Add("Print location of tweet starts...");
-
-            gmcMap.DragButton = MouseButtons.Left;
-            gmcMap.MapProvider = GMapProviders.GoogleMap;
-
-            foreach (Tweet t in twt)
-            {
-                gmcMap.Position = new PointLatLng(t.Coordinates.Latitude, t.Coordinates.Longitude);
-                gmcMap.MinZoom = 5;       // Minimum zoom level
-                gmcMap.MaxZoom = 100;     // Maximum zoom level
-                gmcMap.Zoom = 10;         // Current zoom level
-
-                PointLatLng points = new PointLatLng(t.Coordinates.Latitude, t.Coordinates.Longitude);
-                // Normal marker
-                GMapMarker marker = new GMarkerGoogle(points, GMarkerGoogleType.red);
-
-                // Create an Overlay
-                GMapOverlay markers = new GMapOverlay("markers");
-
-                // Add all available markers to that Overlay
-                markers.Markers.Add(marker);
-
-                // Cover map with Overlay
-                gmcMap.Overlays.Add(markers);
-            }
-
-            
-
-            
-
-            
-            
+            Print_Location_of_Tweet printL = new Print_Location_of_Tweet();
+            printL.Print_Location(twt, gmcMap);
             listBox1.Items.Add("Print location of tweet ends...");
         }
 
